1c1f7a56a6078168d09dd8b73a2fac18fb88a727
Simon Rampp <simon@rampp.dev>
2024-07-26 18:30:51 +0200

    add tempdir to tests

--- tests/test_filter_sweeper.py
+++ tests/test_filter_sweeper.py
@@ -1,6 +1,15 @@
+from pathlib import Path
+import tempfile
+
 from hydra.test_utils.test_utils import TSweepRunner
 import pytest
 from pytest import mark
+
+
+@pytest.fixture
+def temp_dir():
+    with tempfile.TemporaryDirectory() as tmpdir:
+        yield Path(tmpdir)
 
 
 @mark.parametrize(
@@ -14,6 +23,7 @@ def test_filter_configurations(
     hydra_sweep_runner: TSweepRunner,
     config_name: str,
     expected: int,
+    temp_dir,
 ) -> None:
     sweep = hydra_sweep_runner(
         calling_file=__file__,
@@ -22,6 +32,7 @@ def test_filter_configurations(
         config_name=config_name + ".yaml",
         task_function=None,
         overrides=None,
+        temp_dir=temp_dir,
     )
     with sweep:
         assert sweep.returns is not None
@@ -41,6 +52,7 @@ def test_error_types(
     hydra_sweep_runner: TSweepRunner,
     config_name: str,
     raises: Exception,
+    temp_dir,
 ) -> None:
     sweep = hydra_sweep_runner(
         calling_file=__file__,
@@ -49,6 +61,7 @@ def test_error_types(
         config_name=config_name + ".yaml",
         task_function=None,
         overrides=None,
+        temp_dir=temp_dir,
     )
     with pytest.raises(raises):
         with sweep:
